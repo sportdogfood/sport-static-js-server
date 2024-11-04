a215001e705728015583eb0b3547b0688a760382
sportdogfood <philip@sportdogfood.com>
2024-11-04 18:15:54 -0500

    fxz

--- fxsubscriptions.js
+++ fxsubscriptions.js
@@ -39,7 +39,9 @@ async function fetchFoxyCartSubscriptions(customerId) {
         // Ensure userZoom and _embedded exist, then add fx:subscriptions
         window.userZoom = window.userZoom || {};
         window.userZoom._embedded = window.userZoom._embedded || {};
-        window.userZoom._embedded['fx:subscriptions'] = subscriptions.length > 0 ? subscriptions : [];
+
+        // Add fx:subscriptions without overwriting existing embedded data
+        window.userZoom._embedded['fx:subscriptions'] = subscriptions;
 
         // Update session state using the global function
         if (typeof window.updateUserSession === 'function') {
