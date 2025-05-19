45a548a2314d14bec9af17f384aab46f94dce911
sportdogfood <philip@sportdogfood.com>
2025-05-19 15:56:27 -0400

    Create doesNotContain.js

---
+++ modular/doesNotContain.js
@@ -0,0 +1,243 @@
+// doesNotContain.js
+export const doesNotContain = [
+  {
+    name: "Canola",
+    slug: "canola",
+    masterMatch: ["canola", "canola meal"],
+    alternates: []
+  },
+  {
+    name: "Buckwheat",
+    slug: "buckwheat",
+    masterMatch: ["buckwheat"],
+    alternates: []
+  },
+  {
+    name: "White Rice",
+    slug: "white-rice",
+    masterMatch: ["white rice"],
+    alternates: []
+  },
+  {
+    name: "White Potato",
+    slug: "white-potato",
+    masterMatch: ["white potato"],
+    alternates: []
+  },
+  {
+    name: "Plasma",
+    slug: "plasma",
+    masterMatch: ["plasma", "porcine plasma", "dried porcine plasma"],
+    alternates: []
+  },
+  {
+    name: "Potato",
+    slug: "potato",
+    masterMatch: ["potato", "potato fiber", "potato fix", "potato protein", "dried white potatoes", "white potato", "potatoes"],
+    alternates: []
+  },
+  {
+    name: "Barley",
+    slug: "barley",
+    masterMatch: ["barley", "paerled barley"],
+    alternates: []
+  },
+  {
+    name: "Soy",
+    slug: "soy",
+    masterMatch: ["soy", "soybean", "soybean meal", "soy extracts"],
+    alternates: []
+  },
+  {
+    name: "Wheat",
+    slug: "wheat",
+    masterMatch: ["wheat"],
+    alternates: []
+  },
+  {
+    name: "Soybean Oil",
+    slug: "soybean-oil",
+    masterMatch: ["soybean oil"],
+    alternates: []
+  },
+  {
+    name: "By-Product Meal",
+    slug: "by-product-meal",
+    masterMatch: ["by-product meal", "chicken by-product meal", "poultry by-product meal", "by-product", "poultry by"],
+    alternates: []
+  },
+  {
+    name: "Fish Meal",
+    slug: "fish-meal",
+    masterMatch: ["fish meal"],
+    alternates: []
+  },
+  {
+    name: "Corn",
+    slug: "corn",
+    masterMatch: ["corn", "corn flour", "corn germ meal", "corn gluten", "cornmeal"],
+    alternates: []
+  },
+  {
+    name: "Gluten",
+    slug: "gluten",
+    masterMatch: ["gluten", "gluten meal"],
+    alternates: []
+  },
+  {
+    name: "Chickpea",
+    slug: "chickpea",
+    masterMatch: ["chickpea", "chickpeas"],
+    alternates: []
+  },
+  {
+    name: "Lentils",
+    slug: "lentils",
+    masterMatch: ["lentils"],
+    alternates: []
+  },
+  {
+    name: "Canola Oil",
+    slug: "canola-oil",
+    masterMatch: ["canola oil"],
+    alternates: []
+  },
+  {
+    name: "Rice",
+    slug: "rice",
+    masterMatch: ["rice", "rice fix"],
+    alternates: []
+  },
+  {
+    name: "Beans",
+    slug: "beans",
+    masterMatch: ["beans", "bean", "garbanzo"],
+    alternates: []
+  },
+  {
+    name: "Blood Meal",
+    slug: "blood-meal",
+    masterMatch: ["blood meal", "blood meal conventionally dried"],
+    alternates: []
+  },
+  {
+    name: "Vegetable Oil",
+    slug: "vegetable-oil",
+    masterMatch: ["vegetable oil"],
+    alternates: []
+  },
+  {
+    name: "Peas",
+    slug: "peas",
+    masterMatch: ["peas", "pea", "pea fiber", "pea fix", "pea flour", "pea protein"],
+    alternates: []
+  },
+  {
+    name: "Brewers Yeast",
+    slug: "brewers-yeast",
+    masterMatch: ["brewers yeast"],
+    alternates: []
+  },
+  {
+    name: "Citric Acid",
+    slug: "citric-acid",
+    masterMatch: ["citric acid"],
+    alternates: []
+  },
+  {
+    name: "Brewers Rice",
+    slug: "brewers-rice",
+    masterMatch: ["brewers rice"],
+    alternates: []
+  },
+  {
+    name: "Poultry by Product Meal",
+    slug: "poultry-by-product-meal",
+    masterMatch: ["poultry by product meal"],
+    alternates: []
+  },
+  {
+    name: "Poultry Parts",
+    slug: "poultry-parts",
+    masterMatch: ["poultry parts"],
+    alternates: []
+  },
+  {
+    name: "Fish Oil",
+    slug: "fish-oil",
+    masterMatch: ["fish oil"],
+    alternates: []
+  },
+  {
+    name: "Egg",
+    slug: "egg",
+    masterMatch: ["egg"],
+    alternates: []
+  },
+  {
+    name: "Yeast Extracts",
+    slug: "yeast-extracts",
+    masterMatch: ["yeast extracts"],
+    alternates: []
+  },
+  {
+    name: "Beet Pulp",
+    slug: "beet-pulp",
+    masterMatch: ["beet pulp"],
+    alternates: []
+  },
+  {
+    name: "Animal Fat",
+    slug: "animal-fat",
+    masterMatch: ["animal fat"],
+    alternates: []
+  },
+  {
+    name: "Autolyzed Yeast",
+    slug: "autolyzed-yeast",
+    masterMatch: ["autolyzed yeast"],
+    alternates: []
+  },
+  {
+    name: "Powdered Cellulose",
+    slug: "powdered-cellulose",
+    masterMatch: ["powdered cellulose"],
+    alternates: []
+  },
+  {
+    name: "Hydrolyzed Protein",
+    slug: "hydrolyzed-protein",
+    masterMatch: ["hydrolyzed protein"],
+    alternates: []
+  },
+  {
+    name: "Meat by Product",
+    slug: "meat-by-product",
+    masterMatch: ["meat by product"],
+    alternates: []
+  },
+  {
+    name: "Meat by Product Meal",
+    slug: "meat-by-product-meal",
+    masterMatch: ["meat by product meal"],
+    alternates: []
+  },
+  {
+    name: "Meat Parts",
+    slug: "meat-parts",
+    masterMatch: ["meat parts"],
+    alternates: []
+  },
+  {
+    name: "Poultry by Product",
+    slug: "poultry-by-product",
+    masterMatch: ["poultry by product"],
+    alternates: []
+  },
+  {
+    name: "Animal Digest",
+    slug: "animal-digest",
+    masterMatch: ["animal digest"],
+    alternates: []
+  },
+];
