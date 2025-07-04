f417b49310e7ad8ca77a51368243cb186cdf6e40
sportdogfood <philip@sportdogfood.com>
2025-07-04 11:38:57 -0400

    Create vaAgn.js

---
+++ modular/vaAgn.js
@@ -0,0 +1,429 @@
+export const VA_AGNOSTIC = [
+  {
+    "tag": "Calorie Dense",
+    "triggers": [
+      "calorie dense",
+      "calorie",
+      "dense",
+      "calorie-packed",
+      "dog",
+      "food",
+      "high-calorie",
+      "formula",
+      "energy",
+      "for",
+      "weight",
+      "gain",
+      "in",
+      "dogs",
+      "fuel-rich",
+      "kibble",
+      "with",
+      "more",
+      "calories",
+      "per",
+      "cup"
+    ],
+    "ids": [
+      "29280",
+      "29099",
+      "28979"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "High Energy",
+    "triggers": [
+      "high energy",
+      "high",
+      "energy",
+      "energy-boosting",
+      "dog",
+      "food",
+      "for",
+      "active",
+      "dogs",
+      "performance",
+      "that",
+      "burn",
+      "a",
+      "lot",
+      "of",
+      "activity",
+      "levels",
+      "working",
+      "fuel"
+    ],
+    "ids": [
+      "29280"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "High Fat",
+    "triggers": [
+      "high fat",
+      "high",
+      "fat",
+      "high-fat",
+      "content",
+      "dog",
+      "food",
+      "with",
+      "extra",
+      "fat-rich",
+      "performance",
+      "kibble",
+      "for",
+      "dogs",
+      "needing",
+      "more",
+      "calories",
+      "endurance"
+    ],
+    "ids": [
+      "29280"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Highly Digestible",
+    "triggers": [
+      "highly digestible",
+      "highly",
+      "digestible",
+      "easy-to-digest",
+      "dog",
+      "food",
+      "gentle",
+      "on",
+      "digestion",
+      "sensitive",
+      "stomach-friendly",
+      "gut-friendly",
+      "formula"
+    ],
+    "ids": [
+      "29280",
+      "29099",
+      "28979"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Legumes Free",
+    "triggers": [
+      "legumes free",
+      "legumes",
+      "free",
+      "no",
+      "peas",
+      "or",
+      "lentils",
+      "legume-free",
+      "dog",
+      "food",
+      "pea-free",
+      "kibble",
+      "chickpeas",
+      "beans",
+      "excludes",
+      "and",
+      "dry",
+      "with",
+      "of",
+      "lentil-",
+      "formula",
+      "without"
+    ],
+    "ids": [
+      "29280",
+      "29099",
+      "28979"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Poultry Free",
+    "triggers": [
+      "poultry free",
+      "poultry",
+      "free",
+      "chicken-free",
+      "dog",
+      "food",
+      "no",
+      "turkey",
+      "or",
+      "chicken",
+      "poultry-free",
+      "kibble",
+      "non-poultry",
+      "protein",
+      "formula",
+      "excludes",
+      "ingredients",
+      "proteins",
+      "dry",
+      "without",
+      "of",
+      "and",
+      "poultry-sensitive",
+      "legume-free",
+      "recipe"
+    ],
+    "ids": [
+      "29280",
+      "29099",
+      "28979"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Allergy Relief",
+    "triggers": [
+      "allergy relief",
+      "allergy",
+      "relief",
+      "limited",
+      "ingredient",
+      "for",
+      "allergies",
+      "allergy-friendly",
+      "kibble",
+      "food",
+      "itchy",
+      "dogs",
+      "dog",
+      "skin",
+      "sensitivities",
+      "anti-allergy",
+      "formula"
+    ],
+    "ids": [
+      "29280",
+      "29099"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Healthy Skin and Coat",
+    "triggers": [
+      "healthy skin and coat",
+      "healthy",
+      "skin",
+      "and",
+      "coat",
+      "shiny",
+      "dog",
+      "food",
+      "for",
+      "dry",
+      "or",
+      "itchy",
+      "skin-supportive",
+      "kibble",
+      "improves",
+      "condition",
+      "nourishes",
+      "fur"
+    ],
+    "ids": [
+      "29280",
+      "29099",
+      "28979"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Sensitive Stomachs",
+    "triggers": [
+      "sensitive stomachs",
+      "sensitive",
+      "stomachs",
+      "dog",
+      "food",
+      "for",
+      "upset",
+      "stomach",
+      "dogs",
+      "with",
+      "digestion",
+      "issues",
+      "gentle",
+      "formula",
+      "tummy-sensitive",
+      "kibble",
+      "gi",
+      "sensitivity"
+    ],
+    "ids": [
+      "29280",
+      "29099"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Natural Antioxidants",
+    "triggers": [
+      "natural antioxidants",
+      "natural",
+      "antioxidants",
+      "immune-boosting",
+      "dog",
+      "food",
+      "preservative",
+      "formula",
+      "antioxidant-rich",
+      "supports",
+      "immune",
+      "health",
+      "with",
+      "superfoods"
+    ],
+    "ids": [
+      "29280",
+      "29099",
+      "28979"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Performance Minerals",
+    "triggers": [
+      "performance minerals",
+      "performance",
+      "minerals",
+      "dog",
+      "food",
+      "with",
+      "added",
+      "supports",
+      "muscle",
+      "recovery",
+      "and",
+      "strength",
+      "mineral-enriched",
+      "formula",
+      "fortified",
+      "for",
+      "dogs",
+      "electrolyte-balanced",
+      "nutrition"
+    ],
+    "ids": [
+      "29280",
+      "29099",
+      "28979"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Probiotics Added",
+    "triggers": [
+      "probiotics added",
+      "probiotics",
+      "added",
+      "gut",
+      "health",
+      "dog",
+      "food",
+      "digestive",
+      "support",
+      "formula",
+      "includes",
+      "with",
+      "live",
+      "cultures",
+      "promotes",
+      "good",
+      "bacteria"
+    ],
+    "ids": [
+      "29280",
+      "29099",
+      "28979"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Upgraded Vitamins",
+    "triggers": [
+      "upgraded vitamins",
+      "upgraded",
+      "vitamins",
+      "enriched",
+      "with",
+      "vitamin-packed",
+      "formula",
+      "dog",
+      "food",
+      "added",
+      "nutrients",
+      "supports",
+      "immune",
+      "and",
+      "joint",
+      "health",
+      "fortified"
+    ],
+    "ids": [
+      "29280",
+      "29099",
+      "28979"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "500+ kcals per cup",
+    "triggers": [
+      "500+ kcals per cup",
+      "500+",
+      "kcals",
+      "per",
+      "cup"
+    ],
+    "ids": [
+      "29280"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Grain Free",
+    "triggers": [
+      "grain free",
+      "grain",
+      "free"
+    ],
+    "ids": [
+      "28979"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Grain Inclusive",
+    "triggers": [
+      "grain inclusive",
+      "grain",
+      "inclusive"
+    ],
+    "ids": [
+      "29280"
+    ],
+    "description": ""
+  },
+  {
+    "tag": "Peas Free",
+    "triggers": [
+      "peas free",
+      "peas",
+      "free"
+    ],
+    "ids": [
+      "29280",
+      "29099",
+      "28979"
+    ],
+    "description": ""
+  }
+];
