4d6ac81827446a324fb5e761d5381102e43f91ff
sportdogfood <philip@sportdogfood.com>
2025-07-04 11:37:01 -0400

    Create dogAgn.js

---
+++ modular/dogAgn.js
@@ -0,0 +1,238 @@
+
+export const DOG_AGNOSTIC = [
+  {
+    "breed": "Weimaraners",
+    "triggers": [
+      "weimaraners",
+      "weimeraner",
+      "wiemaraner",
+      "weim",
+      "grey",
+      "ghost",
+      "wm",
+      "weims",
+      "gray"
+    ],
+    "ids": [
+      "29099",
+      "28979"
+    ],
+    "activity": "Active",
+    "group": "Field Dogs, Gun Dogs, Bird Dogs, Sporting Dogs, Active Dogs, Performance Dogs, Tracking Dogs, Agility Dogs"
+  },
+  {
+    "breed": "Vizslas",
+    "triggers": [
+      "vizslas",
+      "vizslaa",
+      "vizla",
+      "hungarian",
+      "pointer",
+      "vz",
+      "viz",
+      "red",
+      "dog"
+    ],
+    "ids": [
+      "29099",
+      "28979"
+    ],
+    "activity": "Active",
+    "group": "Field Dogs, Gun Dogs, Bird Dogs, Sporting Dogs, Active Dogs, Performance Dogs, Tracking Dogs, Agility Dogs"
+  },
+  {
+    "breed": "Rottweilers",
+    "triggers": [
+      "rottweilers",
+      "rotweiler",
+      "rottwieler",
+      "rottie",
+      "rtw",
+      "rot"
+    ],
+    "ids": [
+      "29280",
+      "28979"
+    ],
+    "activity": "Highly Active",
+    "group": "K9 Dogs, Working Dogs, Active Dogs, Performance Dogs, Tracking Dogs"
+  },
+  {
+    "breed": "Labrador Retrievers",
+    "triggers": [
+      "labrador retrievers",
+      "labradore",
+      "labrodor",
+      "lab",
+      "lr",
+      "labby"
+    ],
+    "ids": [
+      "29099",
+      "28979"
+    ],
+    "activity": "Active",
+    "group": "Field Dogs, Gun Dogs, Bird Dogs, Sporting Dogs, Active Dogs, Performance Dogs, Tracking Dogs, Agility Dogs"
+  },
+  {
+    "breed": "Golden Retrievers",
+    "triggers": [
+      "golden retrievers",
+      "golden",
+      "retreiver",
+      "goldens",
+      "goldie",
+      "gr",
+      "gold"
+    ],
+    "ids": [
+      "29099",
+      "28979"
+    ],
+    "activity": "Active",
+    "group": "Field Dogs, Gun Dogs, Bird Dogs, Sporting Dogs, Active Dogs, Performance Dogs, Agility Dogs"
+  },
+  {
+    "breed": "German Shorthaired Pointers",
+    "triggers": [
+      "german shorthaired pointers",
+      "german",
+      "shorthair",
+      "gsp",
+      "gshp"
+    ],
+    "ids": [
+      "29099",
+      "28979"
+    ],
+    "activity": "Active",
+    "group": "Field Dogs, Gun Dogs, Bird Dogs, Sporting Dogs, Active Dogs, Performance Dogs, Tracking Dogs, Agility Dogs"
+  },
+  {
+    "breed": "German Shepherds",
+    "triggers": [
+      "german shepherds",
+      "german",
+      "shepard",
+      "sheperd",
+      "gsd",
+      "gs",
+      "shep"
+    ],
+    "ids": [
+      "29280",
+      "28979"
+    ],
+    "activity": "Working",
+    "group": "K9 Dogs, Working Dogs, Active Dogs, Performance Dogs, Tracking Dogs, Agility Dogs"
+  },
+  {
+    "breed": "Dutch Shepherds",
+    "triggers": [
+      "dutch shepherds",
+      "dutch",
+      "shepard",
+      "duch",
+      "shepherd",
+      "hollandse",
+      "herder",
+      "ds",
+      "dutchie"
+    ],
+    "ids": [
+      "29280",
+      "28979"
+    ],
+    "activity": "Working",
+    "group": "K9 Dogs, Working Dogs, Active Dogs, Performance Dogs, Tracking Dogs, Agility Dogs"
+  },
+  {
+    "breed": "Doberman Pinschers",
+    "triggers": [
+      "doberman pinschers",
+      "doberman",
+      "pincher",
+      "dobermann",
+      "dobie",
+      "dp",
+      "dob"
+    ],
+    "ids": [
+      "29280",
+      "28979"
+    ],
+    "activity": "Highly Active",
+    "group": "K9 Dogs, Working Dogs, Active Dogs, Performance Dogs, Tracking Dogs"
+  },
+  {
+    "breed": "Chesapeake Bay Retrievers",
+    "triggers": [
+      "chesapeake bay retrievers",
+      "chessie",
+      "chesapeake",
+      "retriever",
+      "cbr",
+      "chess"
+    ],
+    "ids": [
+      "29099",
+      "28979"
+    ],
+    "activity": "Active",
+    "group": "Field Dogs, Gun Dogs, Bird Dogs, Sporting Dogs, Active Dogs, Performance Dogs, Tracking Dogs"
+  },
+  {
+    "breed": "Border Collies",
+    "triggers": [
+      "border collies",
+      "boarder",
+      "collie",
+      "border",
+      "colie",
+      "bc",
+      "bcollie"
+    ],
+    "ids": [
+      "29099",
+      "28979"
+    ],
+    "activity": "Active",
+    "group": "Working Dogs, Active Dogs, Performance Dogs, Agility Dogs"
+  },
+  {
+    "breed": "Belgian Malinois",
+    "triggers": [
+      "belgian malinois",
+      "belgian",
+      "malinios",
+      "malinwa",
+      "malinois",
+      "mali",
+      "bm",
+      "mal"
+    ],
+    "ids": [
+      "29280",
+      "28979"
+    ],
+    "activity": "Working",
+    "group": "K9 Dogs, Working Dogs, Active Dogs, Performance Dogs, Tracking Dogs, Agility Dogs"
+  },
+  {
+    "breed": "Active\n  Adult",
+    "triggers": [
+      "active\n  adult",
+      "mixed",
+      "any",
+      "aa",
+      "act",
+      "active"
+    ],
+    "ids": [
+      "29099",
+      "28979"
+    ],
+    "activity": "Active",
+    "group": "Active Dogs"
+  }
+];
