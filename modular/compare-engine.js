422c123b7305a22a330cb26202d506533a013088
sportdogfood <philip@sportdogfood.com>
2025-07-14 21:46:36 -0400

    Update compare-engine.js

--- modular/compare-engine.js
+++ modular/compare-engine.js
@@ -446,7 +446,7 @@ function paintSection3(mainRow, sdfRow) {
 }
 
 
-import Typed from "https://cdn.jsdelivr.net/npm/typed.js@2.0.12/dist/typed.umd.js";
+//import Typed from "https://cdn.jsdelivr.net/npm/typed.js@2.0.12/dist/typed.umd.js";
 
 // Helper to safely init Typed.js for a given data-var
 function runTypedForMadlib(dataVar) {
